2d903ac29f530881f6bdea974dc44d8ea47e9fe6
Elem <elem15@yandex.ru>
2023-08-24 22:37:00 +0300

    feat: load first data

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import styles from './page.module.css'
 
-export default function Home() {
+
+export default async function Home() {
+  const response = await fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-08-24&api_key=DEMO_KEY')
+  const data = await response.json()
   return (
-    <div>Main page</div>
+    <div>
+      <h1>Asteroids</h1>
+      <ul>
+        {data['near_earth_objects']['2023-08-29'].map((item) => <li key={item.id}>{item.name}</li>) }
+      </ul>
+    </div>
   )
 }
