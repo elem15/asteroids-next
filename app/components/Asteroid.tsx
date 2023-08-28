0ee83f0470ba69f7624477ee57ef18fb993b91cc
Elem <elem15@yandex.ru>
2023-08-28 21:03:56 +0300

    feat: add loading to button

---
+++ app/components/Asteroid.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+
+type Props = {
+  asteroid: AsteroidOnClient;
+  loading: boolean;
+  addToCart: (asteroid: AsteroidOnClient) => Promise<void>;
+};
+export default function Asteroid({ asteroid, loading, addToCart }: Props) {
+  const [clicked, setClicked] = useState(false);
+  useEffect(() => {
+    if (!loading) {
+      setClicked(false);
+    }
+  }, [loading]);
+  return (
+    <li key={asteroid.id}>
+      <h3>{asteroid.name}</h3>
+      <div>
+        {
+          asteroid.close_approach_date
+        }
+      </div>
+      <div>
+        {asteroid.miss_distance_kilometers} км
+      </div>
+      <div>
+        {Math.floor(asteroid.estimated_diameter_max)} м
+      </div>
+      {asteroid.isInCart ?
+        <button disabled>
+          В корзине
+        </button>
+        :
+        <button onClick={() => { addToCart(asteroid); setClicked(true); }}>
+          {clicked ? 'Загрузка' : 'Заказать'}
+        </button>
+      }
+    </li>);
+}
--- app/components/AsteroidList.tsx
+++ app/components/AsteroidList.tsx
@@ -1,34 +1,15 @@
+import Asteroid from './Asteroid';
+
 type Props = {
   asteroids: AsteroidOnClient[];
   loading: boolean;
   addToCart: (asteroid: AsteroidOnClient) => Promise<void>;
 };
+
 export default function AsteroidList({ asteroids, loading, addToCart }: Props) {
   return (
     <ul>
-      {asteroids.map((item) => <li key={item.id}>
-        <h3>{item.name}</h3>
-        <div>
-          {
-            item.close_approach_date
-          }
-        </div>
-        <div>
-          {item.miss_distance_kilometers} км
-        </div>
-        <div>
-          {Math.floor(item.estimated_diameter_max)} м
-        </div>
-        {item.isInCart ?
-          <button disabled>
-            В корзине
-          </button>
-          :
-          <button onClick={() => addToCart(item)}>
-            Заказать
-          </button>
-        }
-      </li>)}
+      {asteroids.map((item) => <Asteroid key={item.id} asteroid={item} loading={loading} addToCart={addToCart} />)}
     </ul>
   );
 }
