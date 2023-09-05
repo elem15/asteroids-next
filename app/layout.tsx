0940abc18366e8c444a15b35c90b549321025c34
Elem <elem15@yandex.ru>
2023-09-05 16:26:50 +0300

    fix: title and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,9 @@ import { useEffect, useRef, useState } from 'react';
 import Header from '../app/components/header/Header';
 import Image from 'next/image';
 import './globals.css';
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin', 'cyrillic'] });
-
-export const metadata: Metadata = {
-  title: 'Armageddon',
-  description: 'Save the planet from asteroids',
-};
-
 
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
   const [isEarthStatic, setIsEarthStatic] = useState(true);
@@ -43,10 +36,16 @@ export default function RootLayout({ children }: { children: React.ReactNode; })
 
   return (
     <html lang="ru">
+      <head>
+        <title>Armageddon</title>
+        <meta name="description" content="Save the planet from asteroids" />
+        <meta name="darkreader-lock"></meta>
+      </head>
       <body className={inter.className}>
         <Header />
         <div ref={observerTargetEarth}></div>
-        <Image className={isEarthStatic ? "earth earth__up" : "earth"} src="/img/planeta_zemlia.jpg" alt="earth" width={400} height={620} />
+        <Image className={isEarthStatic ? "earth earth__up" : "earth"} src="/img/planeta_zemlia.jpg" alt="earth"
+          width={400} height={620} priority={false} />
         <div>{children}</div>
       </body>
     </html>
